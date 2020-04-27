a071349881417fb307e9fa40420fa4841632a4aa
Mark Lee <mark.lee@capricasoftware.co.uk>
2020-04-27 16:28:34 +0100

    Go back to Java 1.8 without modules as the baseline
    An automatic module name is provided.
    
    Java Modules is just too much burden for basically no benefit.

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module uk.co.caprica.vlcj.examples {
-    requires uk.co.caprica.vlcj;
-    requires uk.co.caprica.vlcj.natives;
-    requires java.desktop;
-    requires com.sun.jna;
-    requires com.sun.jna.platform;
-}
-
