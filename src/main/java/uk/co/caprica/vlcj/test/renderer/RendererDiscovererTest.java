577805af97dc119598f2cdb8fc8682d6ef2f3a66
Mark Lee <mark.lee@capricasoftware.co.uk>
2022-01-13 11:15:56 +0000

    Update example to latest API

--- src/main/java/uk/co/caprica/vlcj/test/renderer/RendererDiscovererTest.java
+++ src/main/java/uk/co/caprica/vlcj/test/renderer/RendererDiscovererTest.java
@@ -71,7 +71,7 @@ public class RendererDiscovererTest {
                     // longer need it)
                     itemAdded.hold();
 
-                    boolean result = mediaPlayer.setRenderer(itemAdded);
+                    boolean result = mediaPlayer.renderer().setRenderer(itemAdded);
                     System.out.println("result of set renderer is " + result);
 
                     result = mediaPlayer.media().play(mrl);
