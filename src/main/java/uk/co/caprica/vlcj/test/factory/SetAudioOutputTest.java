3479b64c5d97df86128b47591c9ef24adee24fdf
Mark Lee <mark.lee@capricasoftware.co.uk>
2026-03-01 17:40:34 +0000

    Remove an obsolete comment

--- src/main/java/uk/co/caprica/vlcj/test/factory/SetAudioOutputTest.java
+++ src/main/java/uk/co/caprica/vlcj/test/factory/SetAudioOutputTest.java
@@ -41,9 +41,6 @@ import java.util.List;
  * Specify a single MRL on the command-line.
  * <p>
  * Select an audio output from the menu and play the media.
- * <p>
- * <strong>This does NOT work because of a bug or incomplete implementation in libvlc
- * currently.</strong>
  */
 public class SetAudioOutputTest extends VlcjTest {
 
